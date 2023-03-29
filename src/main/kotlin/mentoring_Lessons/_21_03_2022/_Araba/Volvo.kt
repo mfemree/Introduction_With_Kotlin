1607cd0b1fd83ae7ccbe8c6754a201fd62bb4023
mfemree <usmustafaemre@gmail.com>
2023-03-29 13:42:33 +0200

    changes

--- src/main/kotlin/mentoring_Lessons/_21_03_2022/_Araba/Volvo.kt
+++
@@ -1,9 +0,0 @@
-package mentoring_Lessons._21_03_2022._Araba
-
-class Volvo: Araba4("mavi", 2000) {
-
-    var yil : Int = this.sene
-
-    var renkk: String = this.renk   // renkk = yukardaki renk olsun
-
-}
